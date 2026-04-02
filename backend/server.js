7a6b6a357347bc6e3f9163082011dc0d7aff9ad9
Nivrutti Tidke <nivrutitidke108@gmail.com>
2026-04-02 18:34:56 +0530

    fic CORS

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 app.use(cors({
     origin:[
         "http://localhost:5173",
-        "http://chatnest.vercel.app",
+        "https://chatnest.vercel.app",
         "https://chatnest-ag0g.onrender.com"
         ],
     credentials: true,
