c4979b9976ed0c173e26a4883da77065606be949
Nivrutti Tidke <nivrutitidke108@gmail.com>
2026-04-02 19:06:36 +0530

    fix depolyment error

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,9 @@ app.use(cors({
     origin:[
         "http://localhost:5173",
         "https://chatnest-eta.vercel.app/",
-        
         ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
