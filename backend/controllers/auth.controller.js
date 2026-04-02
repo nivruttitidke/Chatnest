b6c97e2688cc0521a42cc8ae895ab2899b32d913
Nivrutti Tidke <nivrutitidke108@gmail.com>
2026-04-02 18:30:54 +0530

    fic cookie settings for production

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -51,7 +51,9 @@ try {
     res.cookie("jwt",token,{
     maxAge:7 * 24 * 60 * 60 * 1000,
     httpOnly:true,
-    sameSite:"strict",
+    sameSite:process.env.NODE_ENV === "production"
+      ? "none"
+      : "lax",
     secure:process.env.NODE_ENV === "production",
     })
    
