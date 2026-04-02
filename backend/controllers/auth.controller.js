b5ca77c2559f3e8d08097622213b1d1bcf02ccbb
Nivrutti Tidke <nivrutitidke108@gmail.com>
2026-04-02 18:52:54 +0530

    fix depolyment error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -50,11 +50,9 @@ try {
 
     res.cookie("jwt",token,{
     maxAge:7 * 24 * 60 * 60 * 1000,
-    httpOnly:true,
-    sameSite:process.env.NODE_ENV === "production"
-      ? "none"
-      : "lax",
-    secure:process.env.NODE_ENV === "production",
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
     })
    
     res.status(201).json({success:true, user:newUser});
